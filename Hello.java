afc6b18fbf409da71cd1fcb96e4bd6d0624d9422
woongbini <gamebook1004@gmail.com>
2016-12-01 15:53:52 +0900

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, World! from GIT!");
+      System.out.println("change after first commit~");
    }
 }
