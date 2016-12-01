068fca5a7fbfca8ee5e999a7f6612b4bc05a0510
woongbini <gamebook1004@gmail.com>
2016-12-01 16:19:41 +0900

    4th commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, World! from GIT!");
       System.out.println("change after first commit~");
+      System.out.println("change after push~");
    }
 }
